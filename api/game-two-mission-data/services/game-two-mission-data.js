c4f8f23a7c80753532737879954ea46ef2385cd8
AmanajasLG <luizamanajas@hotmail.com>
2021-04-28 14:31:18 -0300

    ADD service game 2 mission data

--- api/game-two-mission-data/services/game-two-mission-data.js
+++ api/game-two-mission-data/services/game-two-mission-data.js
@@ -5,4 +5,24 @@
  * to customize this service
  */
 
-module.exports = {};
+module.exports = {
+  find(params, populate) {
+    let p = [
+      'locations',
+      'questions',
+      { path: 'missionCharacters', populate: [{ path: 'character', populate: ['characterAssets'] }, 'answers'] },
+      {path: 'tutorial', populate: [{path: 'character', populate: ['characterAssets']}, 'location']}
+    ]
+    return strapi.query('game-two-mission-data').find(params, p);
+  },
+
+  findOne(params, populate) {
+    let p = [
+      'locations',
+      'questions',
+      { path: 'missionCharacters', populate: [{ path: 'character', populate: ['characterAssets'] }, 'answers'] },
+      {path: 'tutorial', populate: [{path: 'character', populate: ['characterAssets']}, 'location']}
+    ]
+    return strapi.query('game-two-mission-data').findOne(params, p);
+  },
+};
