19a9b1e5b3981af47f50777a565ad5b6c2371c53
AmanajasLG <luizamanajas@hotmail.com>
2021-08-04 18:30:28 -0300

    FIX game 7 result

--- api/game-routes/controllers/game-routes.js
+++ api/game-routes/controllers/game-routes.js
@@ -347,12 +347,12 @@ module.exports = {
           results: await strapi.query("game-six-result").create(ctx.query),
         };
       case 7:
+        if (ctx.query.userErrors)
+          ctx.query.userErrors = JSON.parse(ctx.query.userErrors);
         return {
           results: await strapi.query("game-seven-result").create(ctx.query),
         };
       case 8:
-        if (ctx.query.userErrors)
-          ctx.query.userErrors = JSON.parse(ctx.query.userErrors);
         return {
           results: await strapi.query("game-eight-result").create(ctx.query),
         };
