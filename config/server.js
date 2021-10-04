7f618f4f8e942c0707910c3dffc37c66934c4990
AmanajasLG <luizamanajas@hotmail.com>
2021-10-04 16:15:47 -0300

    ATT server

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  host: "0.0.0.0",
-  port: process.env.PORT || 5000,
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET", "15889532abb797000acf0f2ddb38a5d0"),
